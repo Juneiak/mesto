a6d014ac27b44a005a84540874bd1414ac802037
Alan I <juneiak@gmail.com>
2021-03-15 20:35:37 +0300

    fix: first review

--- scripts/base-script.js
+++ scripts/base-script.js
@@ -4,30 +4,28 @@ let popUpCloseButton = document.querySelector('.pop-up__close-button');
 let formElement = document.querySelector('.pop-up__form');
 let nameInput = document.querySelector('#name');
 let aboutInput = document.querySelector('#about');
+let profileName = document.querySelector('.profile__name');
+let profileAbout = document.querySelector('.profile__about');
 
-
-function OpenPopUp() {
-    let profileName = document.querySelector('.profile__name');
-    let profileAbout = document.querySelector('.profile__about');
+function openPopUp() {
+    
     nameInput.value = profileName.textContent;
     aboutInput.value = profileAbout.textContent;
     popUp.classList.add('pop-up_opened');
 }
 
-function ClosePopUp() {
+function closePopUp() {
     popUp.classList.remove('pop-up_opened');
 }
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    let profileName = document.querySelector('.profile__name');
-    let profileAbout = document.querySelector('.profile__about');
-    profileName.textContent = `${nameInput.value}`;
-    profileAbout.textContent = `${aboutInput.value}`;
+    profileName.textContent = nameInput.value;
+    profileAbout.textContent = aboutInput.value;
     popUp.classList.remove('pop-up_opened');
 }
 
 
 formElement.addEventListener('submit', formSubmitHandler);
-profileEditButton.addEventListener('click', OpenPopUp);
-popUpCloseButton.addEventListener('click', ClosePopUp);
+profileEditButton.addEventListener('click', openPopUp);
+popUpCloseButton.addEventListener('click', closePopUp);
