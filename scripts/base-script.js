26175560da01175adb4f977dd8906461746cb566
Alan I <juneiak@gmail.com>
2021-03-25 02:02:55 +0300

    feat: add photo by js

--- scripts/base-script.js
+++ scripts/base-script.js
@@ -1,11 +1,11 @@
-let editPopUp = document.querySelector('#edit');
-let profileEditButton = document.querySelector('.profile__edit-button');
-let editPopUpCloseButton = document.querySelector('#close-edit');
-let editFormElement = document.querySelector('#edit-form');
-let nameInput = document.querySelector('#name');
-let aboutInput = document.querySelector('#about');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
+const editPopUp = document.querySelector('#edit');
+const profileEditButton = document.querySelector('.profile__edit-button');
+const editPopUpCloseButton = document.querySelector('#close-edit');
+const editFormElement = document.querySelector('#edit-form');
+const nameInput = document.querySelector('#name');
+const aboutInput = document.querySelector('#about');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
 
 function openEditPopUp() {
     nameInput.value = profileName.textContent;
@@ -31,10 +31,10 @@ editPopUpCloseButton.addEventListener('click', () => closePopUp(editPopUp));
 
 
 
-let addPopUp = document.querySelector('#add');
-let photoAddButton = document.querySelector('.profile__add-button');
-let addFormElement = document.querySelector('#add-form');
-let addPopUpCloseButton = document.querySelector('#close-add');
+const addPopUp = document.querySelector('#add');
+const photoAddButton = document.querySelector('.profile__add-button');
+const addFormElement = document.querySelector('#add-form');
+const addPopUpCloseButton = document.querySelector('#close-add');
 
 //function addFormSubmitHandler() {}
 
@@ -43,4 +43,44 @@ photoAddButton.addEventListener('click', () => {addPopUp.classList.add('pop-up_o
 addPopUpCloseButton.addEventListener('click', () => closePopUp(addPopUp));
 
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
 
+
+function addPastPhotos(database) {
+    const elementTemplate = document.querySelector('#element').content;
+    const elementsTable = document.querySelector('.elements__table');
+    for (let i = 0; i < database.length; i++) {
+        let photoElement = elementTemplate.querySelector('.element').cloneNode(true);
+        photoElement.querySelector('.element__image').src = database[i].link;
+        photoElement.querySelector('.element__image').alt = `фотография ${database[i].name}`;
+        photoElement.querySelector('.element__place-name').textContent = database[i].name;
+        elementsTable.prepend(photoElement);
+    }
+}
+
+addPastPhotos(initialCards)
